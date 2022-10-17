8a3a2164df1136690f1cca03d0565573d7b25f09
Guilherme Puida <guilhermepmoreira00@gmail.com>
2022-10-17 01:04:26 -0300

    Add links to post headings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    extendDefaultPlugins: true,
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: "wrap" }]]
+  },
   integrations: [
     tailwind({
       config: {
